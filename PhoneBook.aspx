b2cd56247c5dbdb3139dab4e9f8d4020d0bd437e
Tonny Boesenbaek <tonnyb@gmail.com>
2016-07-25 09:23:08 -0400

    Done

---
+++ PhoneBook.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="PhoneBook.aspx.cs" Inherits="PhoneBook" Async="true" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 21%;
+        }
+        .auto-style2 {
+            width: 116px;
+        }
+        .auto-style4 {
+            width: 112px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <p>
+        <h1>Phone Book</h1>
+
+        <p>Add an entry to the phone book:</p>
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">Last name:</td>
+                <td>
+                    <asp:TextBox ID="txtLastName" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">First name:</td>
+                <td>
+                    <asp:TextBox ID="txtFirstName" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Phone number:</td>
+                <td>
+                    <asp:TextBox ID="txtPhoneNumber" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+        </table>
+        <asp:Button ID="btnAddEntry" runat="server" Text="Add Phone Book Entry" Width="273px" OnClick="btnAddEntry_Click" />
+        
+        <p>
+            Locate entries in the phone book:
+
+        </p>
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style4">Last name:</td>
+                <td>
+                    <asp:TextBox ID="txtFindLastName" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+        </table>
+        <asp:Button ID="btnFind" runat="server" Text="Find Entries by Last Name" Width="269px" OnClick="btnFind_Click"/>
+
+        <p>
+            Results:<br />
+        
+            <asp:TextBox ID="txtResult" runat="server" Height="150px" TextMode="MultiLine" Width="344px"></asp:TextBox>
+        </p>
+    </form>
+</body>
+</html>
---
+++ PhoneBook.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Http;
+using System.Xml.Linq;
+using System.Xml;
+
+public partial class PhoneBook : System.Web.UI.Page
+{
+    // object to invoke the web serivce
+    HttpClient client = new HttpClient();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+
+    protected async void btnFind_Click(object sender, EventArgs e)
+    {
+        // clear any text in the result textfield
+        txtResult.Text = string.Empty; 
+
+        string personToFind = txtFindLastName.Text;
+        Uri uri = new Uri("http://localhost:25955/Service1.svc/" + personToFind);
+
+        string result = await client.GetStringAsync(uri);
+
+        // Parse the returned xml
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(result);
+
+        XmlNodeList node;
+
+        node = xmlDoc.GetElementsByTagName("string");
+
+        // Add query result to Result text box
+        for (int i = 0; i < node.Count; i++)
+        {
+            txtResult.Text += node[i].InnerText + "\r\n";
+        }
+        
+    }
+
+    protected async void btnAddEntry_Click(object sender, EventArgs e)
+    {
+        // Get user input
+        string firstName = txtFirstName.Text;
+        string lastName = txtLastName.Text;
+        string phoneNumber = txtPhoneNumber.Text;
+
+        Uri uri = new Uri("http://localhost:25955/Service1.svc/" + lastName + "/" + firstName + "/" + phoneNumber);
+
+        string result = string.Empty;
+        // String 'result' should contain the text: "Entry added sucessfully"
+        try
+        {
+            result = await client.GetStringAsync(uri);
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(result);
+            XmlNodeList node;
+
+            node = xmlDoc.GetElementsByTagName("string");
+
+            // clear any text in the result textfield
+            txtResult.Text = string.Empty;
+            txtResult.Text = node[0].InnerText;
+        }
+        catch (HttpRequestException ex)
+        {
+            txtResult.Text = string.Empty;
+            txtResult.Text = "Something went wrong on the frontend.\nDetailed error message:\n" + ex.InnerException.ToString();
+        }
+    }
+}
